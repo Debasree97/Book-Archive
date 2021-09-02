b4dc778e7f28a079ba056a7a29dc1006d59872a5
Debasree97 <atd21821@gmail.com>
2021-09-02 22:50:14 +0600

    modify

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const searchBook = () => {
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
     searchField.value = "";
-    const url = ` http://openlibrary.org/search.json?q=${searchText}`;
+    const url = ` https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => searchResult(data));
